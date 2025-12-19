9304895e440e88910f8e89d637eb966115a17d42
simondlevy <simon.d.levy@gmail.com>
2025-12-19 17:37:23 -0500

    modified:   src/sensors/rangefinder.hpp

--- src/sensors/rangefinder.hpp
+++ src/sensors/rangefinder.hpp
@@ -33,9 +33,13 @@ class SimRangefinder {
 
     public:
 
-        void read(const vector<Wall *> walls, int * distances_mm)
+        void read(const vec3_t & robot_position, const vector<Wall *> walls,
+                int * distances_mm)
         {
-            (void)walls;
+            printf("x=%+3.3fm y=%+3.3fm z=%+3.3fm\n",
+                    robot_position.x, robot_position.y, robot_position.z);
+
+            walls[0]->dump();
 
             for (int k=0; k<this->width; ++k) {
                 distances_mm[k*this->height+k] = -1;
