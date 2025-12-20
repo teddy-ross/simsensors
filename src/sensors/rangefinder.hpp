3c587337e2b85b268546ced6b2c212616d5b80f7
simondlevy <simon.d.levy@gmail.com>
2025-12-20 14:54:54 -0500

    modified:   src/sensors/rangefinder.hpp

--- src/sensors/rangefinder.hpp
+++ src/sensors/rangefinder.hpp
@@ -18,6 +18,8 @@
 
 #pragma once
 
+#include <math.h>
+
 namespace simsens {
 
     class SimRangefinder {
@@ -27,42 +29,52 @@ namespace simsens {
 
         private:
 
-        int width;
-        int height; 
-        int min_distance_mm;
-        int max_distance_mm;
-        double field_of_view_radians;
+            static constexpr double MAX_WORLD_SIZE_M = 1000; // arbitrary
+
+            int width;
+            int height; 
+            int min_distance_mm;
+            int max_distance_mm;
+            double field_of_view_radians;
 
         public:
 
-        void read(const pose_t & robot_pose, const vector<Wall *> walls,
-                int * distances_mm)
-        {
-            printf("x=%+3.3fm y=%+3.3fm z=%+3.3fm | ",
-                    robot_pose.x, robot_pose.y, robot_pose.z);
+            void read(const pose_t & robot_pose, const vector<Wall *> walls,
+                    int * distances_mm, double & end_x, double & end_y)
+            {
+                (void)walls;
+                /*
+                printf("x=%+3.3fm y=%+3.3fm z=%+3.3fm | ",
+                        robot_pose.x, robot_pose.y, robot_pose.z);
+                printf("phi=%+3.3f theta=%+3.3f psi=%+3.3f\n",
+                        robot_pose.phi, robot_pose.theta, robot_pose.psi);
+                walls[0]->dump();
+                        */
 
-            printf("phi=%+3.3f theta=%+3.3f psi=%+3.3f\n",
-                    robot_pose.phi, robot_pose.theta, robot_pose.psi);
+                const double max_distance_m = 0.1;//this->max_distance_mm / 1000;
 
-            walls[0]->dump();
+                end_x = robot_pose.x + cos(robot_pose.psi) * max_distance_m;
+                end_y = robot_pose.y - sin(robot_pose.psi) * max_distance_m;
 
-            for (int k=0; k<this->width; ++k) {
-                distances_mm[k*this->height+k] = -1;
+
+                // XXX show a diagonal pattern for now
+                for (int k=0; k<this->width; ++k) {
+                    distances_mm[k*this->height+k] = -1;
+                }
             }
-        }
 
-        void dump()
-        {
-            printf("Rangefinder: \n");
+            void dump()
+            {
+                printf("Rangefinder: \n");
 
-            printf("  fov: %3.3f rad\n", field_of_view_radians);
-            printf("  width: %d\n", width);
-            printf("  height: %d\n", height);
-            printf("  min range: %d mm\n", min_distance_mm);
-            printf("  max range: %d mm\n", max_distance_mm);
+                printf("  fov: %3.3f rad\n", field_of_view_radians);
+                printf("  width: %d\n", width);
+                printf("  height: %d\n", height);
+                printf("  min range: %d mm\n", min_distance_mm);
+                printf("  max range: %d mm\n", max_distance_mm);
 
-            printf("\n");
-        }
+                printf("\n");
+            }
     };
 
 }
