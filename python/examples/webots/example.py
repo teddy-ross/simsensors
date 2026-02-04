e206fde20a20628e8c9859005c47c72e8560e7d1
Simon D. Levy <simon.d.levy@gmail.com>
2026-02-04 13:03:56 -0500

    new file:   python/examples/webots/example.py

---
+++ python/examples/webots/example.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+'''
+   Webots world-parsing tester
+
+   Copyright (C) 2025 Simon D. Levy
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, in version 3.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program. If not, see <http:--www.gnu.org/licenses/>.
+'''
+
+from sys import argv
+
+if len(argv) < 3:
+    print('Usage: %s WORLDFILE ROBOTFILE' % argv[0]);
+    exit(1)
+
+
+'''
+int main(int argc, char ** argv) 
+{
+    if (argc < 3) {
+    }
+
+    else {
+
+        simsens::World world = {};
+        simsens::WorldParser::parse(argv[1], world);
+        world.report();
+
+        simsens::Robot robot = {};
+        simsens::RobotParser::parse(argv[2], robot);
+        robot.report();
+    }
+
+    return 0;
+}
+'''
